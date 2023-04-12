b3883807e031d7cb6c9106bfe0d30912f871aa72
Milosz Dzierzawski <240656@edu.p.lodz.pl>
2023-04-12 13:42:54 +0200

    Refaktoryzacja stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,7 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int SIZE = 12;
+    public static final int maxSize = 11;
+    public static final int badIndex = -1;
+    private int[] numbers = new int[SIZE];
 
     public int total = -1;
 
@@ -12,11 +15,11 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == badIndex;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == maxSize;
     }
 
     protected int peekaboo() {
