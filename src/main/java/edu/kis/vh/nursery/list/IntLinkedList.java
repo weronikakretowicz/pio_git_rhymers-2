da696987df843964b84e80726c402f76e12f7abc
Weronika Kretowicz <235903@edu.p.lodz.pl>
2023-05-19 21:32:23 +0200

    Wygenerowanie szkieletu dokumentacji dla klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -6,6 +6,10 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     * It pushes value 'i' to the list, if it's empty creates new list and add value
+     * @param i value to be added
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -16,22 +20,38 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * It checks if list is empty
+     * @return Returns true if list is empty
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * It checks if list is full
+     * @return Returns false becouse it's a dynamic structure, so it will never be full
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * It returns top value in the list
+     * @return Returns IF_EMPTY_RETURN_VAL or the last value in the list
+     */
     public int top() {
         if (isEmpty())
-
             return IF_EMPTY_RETURN_VAL;
+
         return last.value;
 
     }
 
+    /**
+     * It removes the last element from the list
+     * @return Returns IF_EMPTY_RETURN_VAL if list is empty or return the last value and set pointer (last) to the previous node
+     */
     public int pop() {
         if (isEmpty())
 
